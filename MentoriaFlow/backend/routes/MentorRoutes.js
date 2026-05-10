0a1034e343dc99af8cc264f49d6c3c602010a950
Pietro Costa Cardoso <pietrocosta.cardoso1@gmail.com>
2026-05-10 10:43:30 -0300

    Clarify API routes with a comment
    Added a comment to clarify the purpose of the routes.

--- MentoriaFlow/backend/routes/MentorRoutes.js
+++ MentoriaFlow/backend/routes/MentorRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 
 const MentorController = require('../controllers/MentorController')
 
-// middl
+// Define as rotas de API
 const verifyToken = require('../helpers/check-token')
 const { imageUpload } = require('../helpers/image-upload')
 
