fa3b8016627985bda84d9da8d15efdf78e9f8633
Pietro Costa Cardoso <pietrocosta.cardoso1@gmail.com>
2026-06-21 22:38:49 -0300

    Correct comment style in UserRoutes.js
    Fix comment formatting for consistency.

--- MentoriaFlow/backend/routes/UserRoutes.js
+++ MentoriaFlow/backend/routes/UserRoutes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const UserController = require("../controllers/UserController");
 
-// middlewares
+// middlewares.
 const verifyToken = require("../helpers/check-token");
 const { imageUpload } = require("../helpers/image-upload");
 
